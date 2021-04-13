3bd1c96c2bcc4d8d9382c6b260be3ee1d0e61649
Jack-w-DOS <jack_w@directonlineservices.co.uk>
2021-04-13 19:55:34 +0100

    Moved components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Layout from "./components/Layout"
-import Navigation from "./components/Navigation"
+import Layout from "../components/Layout"
+import Navigation from "../components/Navigation"
 
 export default function Home() {
   return (
