a06dc53a2d67a03c7eb5b9b64deb43e4812c1478
Jack-w-DOS <jack_w@directonlineservices.co.uk>
2021-06-19 12:18:36 +0100

    Updated reviews section, no longer needs useEffect

--- src/components/HomeComponents/ReviewSection/index.js
+++ src/components/HomeComponents/ReviewSection/index.js
@@ -1,45 +1,74 @@
-import React, { useEffect, useState } from "react"
-import { Helmet } from 'react-helmet'
+import React from "react"
+import { Helmet } from "react-helmet"
 import * as style from "./review-section.module.scss"
 
 import Button from "../../Button"
 import Box from "../../Box"
 import AnimateOnScroll from "../../AnimateOnScroll"
 
-export default function ReviewSection() {
-    const [addScript, setAddScript] = useState(false);
-    useEffect(() => {
-        setAddScript(true);
-    }, [])
-    return (
-        <>
-            {addScript && (
-                <Helmet>
-                    <script async src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=748&amp;locationId=19141578&amp;lang=en_UK&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=false&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
-                </Helmet>
-            )}
-            <div className={style.ReviewSection}>
-                <div className="container">
-                    <div className="grid grid--gap">
-                        <div className="grid__12 grid__md-6">
-                            <h2>Don’t just take our word for it</h2>
-                            <p>
-                                We think our pizzas are some of the best around, but we dont expect you to take our word for it. Take a look at some of our reviews from trip advisor.
-                            </p>
-                            <Button>Find us on trip advisor</Button>
-                        </div>
-                        <div className="grid__12 grid__md-6">
-                            <AnimateOnScroll from="bottom" rootMargin="100px 0px" animateOut={false}>
-                                <div className={style.ReviewSection_box}>
-                                    <Box>
-                                        <div id="TA_selfserveprop748" className={`TA_selfserveprop ${style.ReviewSection_box_widget}`}><ul id="CEkFmKrMfu1" className="TA_links O3DeLF1BMrf1"><li id="VeTaib" className="8ASPMz2bzQ"><a target="_blank" rel="noreferrer" href="https://www.tripadvisor.co.uk/Restaurant_Review-g187047-d19141578-Reviews-Hooker_Eight-Gloucester_Cotswolds_England.html"><img src="https://www.tripadvisor.co.uk/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-11900-2.svg" alt="TripAdvisor"/></a></li></ul></div>
-                                    </Box>
-                                </div>
-                            </AnimateOnScroll>
-                        </div>
+const ReviewSection = () => (
+    <>
+        <Helmet>
+            <script
+                async
+                src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=748&amp;locationId=19141578&amp;lang=en_UK&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=false&amp;display_version=2"
+                data-loadtrk
+                onload="this.loadtrk=true"
+            ></script>
+        </Helmet>
+        <div className={style.ReviewSection}>
+            <div className="container">
+                <div className="grid grid--gap">
+                    <div className="grid__12 grid__md-6">
+                        <h2>Don’t just take our word for it</h2>
+                        <p>
+                            We think our pizzas are some of the best around, but
+                            we dont expect you to take our word for it. Take a
+                            look at some of our reviews from trip advisor.
+                        </p>
+                        <Button>Find us on trip advisor</Button>
+                    </div>
+                    <div className="grid__12 grid__md-6">
+                        <AnimateOnScroll
+                            from="bottom"
+                            rootMargin="100px 0px"
+                            animateOut={false}
+                        >
+                            <div className={style.ReviewSection_box}>
+                                <Box>
+                                    <div
+                                        id="TA_selfserveprop748"
+                                        className={`TA_selfserveprop ${style.ReviewSection_box_widget}`}
+                                    >
+                                        <ul
+                                            id="CEkFmKrMfu1"
+                                            className="TA_links O3DeLF1BMrf1"
+                                        >
+                                            <li
+                                                id="VeTaib"
+                                                className="8ASPMz2bzQ"
+                                            >
+                                                <a
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                    href="https://www.tripadvisor.co.uk/Restaurant_Review-g187047-d19141578-Reviews-Hooker_Eight-Gloucester_Cotswolds_England.html"
+                                                >
+                                                    <img
+                                                        src="https://www.tripadvisor.co.uk/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-11900-2.svg"
+                                                        alt="TripAdvisor"
+                                                    />
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </Box>
+                            </div>
+                        </AnimateOnScroll>
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    </>
+)
+
+export default ReviewSection
